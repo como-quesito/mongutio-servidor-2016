d19ae7ec1910915c3dc9a8bc1aa44c89a5fbcb8b
como-quesito <celiesita1951>
2016-06-15 16:32:52 -0500

    aaa

--- src/main/java/org/campitos/MongutioApplication.java
+++ src/main/java/org/campitos/MongutioApplication.java
@@ -1,15 +1,32 @@
 package org.campitos;
 
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.gridfs.GridFsOperations;
+import org.springframework.data.mongodb.gridfs.GridFsTemplate;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
 
 @SpringBootApplication
 public class MongutioApplication implements CommandLineRunner {
 
 	@Autowired
 	private  RepositorioCliente repo;
+
+	/*
+	Con la configuracion ehcha antes sí funcona este que es el mongotemplate
+	 */
+	@Autowired
+	MongoTemplate template;
+
+	@Autowired
+	GridFsTemplate grid;
 
 	public static void main(String[] args) {
 		SpringApplication.run(MongutioApplication.class, args);
@@ -27,6 +44,24 @@ public class MongutioApplication implements CommandLineRunner {
 
 		for(Cliente cli:repo.findAll()){
 			System.out.println("Cliente con nombre:"+cli.getNombre());
+
+			System.out.println("Ya todo");
+		}
+   //Para el mongo template
+		template.save(new Cliente("Cacurrio", 45));
+
+		//Para grifs	C:\Users\rapid\Pictures
+		InputStream inputStream = null;
+		DBObject metaData = new BasicDBObject();
+		metaData.put("extra1", "anything 1");
+		metaData.put("extra2", "anything 2");
+
+		try {
+			inputStream = new FileInputStream("C:\\Users\\rapid\\Pictures\\alnmacig.jpg");
+			//grid.store(inputStream, "alnmacig.jpg", "image/jpg", metaData); si funciono
+
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
 		}
 
 	}
